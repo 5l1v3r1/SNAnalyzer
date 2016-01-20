5d3d5416a69909ca3179ab02c1c73adfd885d1f0
Furkan Kalkan <furkan.kalkan3@gmail.com>
2016-01-20 20:22:03 +0200

    Added {user,relation} add/remove/check

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 import sna
 SNA = sna.SNAnalyzer(inPath="example_network.txt",outPath="out/example_network.gv",engine="circo",analyzeType="tw")
+SNA.addUser("Fenasi Kerim")
+SNA.addUser("Nuri Alço")
+SNA.addRelation("Fenasi Kerim","Nuri Alço")
+SNA.addRelation("Nuri Alço","Fenasi Kerim")
 SNA.renderGraph()
--- sna.py
+++ sna.py
@@ -11,6 +11,7 @@ class SNAnalyzer(object):
             self.outPath = outPath
             self.engine = engine
             self.aType = analyzeType
+            self.networkFileParse(self.inPath)
 
     def random_id(self,len):
         alpha = 'abcdefghijklmnopqrstuvwxyz'
@@ -27,9 +28,8 @@ class SNAnalyzer(object):
             self.network.update({line.split(":")[0]:e.split(",") for e in line.split(":")[1:]})
         f.close()
 
-    def graphUpdate(self):
-            self.networkFileParse(self.inPath)
 
+    def graphUpdate(self):
             if self.aType == "tw":
                 self.graph = Digraph()
             elif self.aType == "fb":
@@ -52,3 +52,36 @@ class SNAnalyzer(object):
         self.graphUpdate()
         self.graph.engine = self.engine
         self.graph.render(self.outPath, view=True)
+        print(self.network)
+
+    def addUser(self,newUser):
+        if self.aType == "tw":
+            if newUser not in self.nameIndex.keys():
+                self.nameIndex[newUser] = self.random_id(3)
+                self.network[newUser] = []
+                print(self.nameIndex)
+
+    def removeUser(self,targetUser):
+        if targetUser in self.nameIndex.keys():
+            del self.nameIndex[targetUser]
+            del self.network[targetUser]
+            for key in self.network.keys():
+                if targetUser in self.network[key]:
+                    self.network[key].remove(targetUser)
+
+
+    def hasRelation(self,sourceUser,targetUser):
+        friends = self.network[sourceUser]
+        if targetUser in friends:
+            return True
+        else:
+            return False
+
+    def addRelation(self,sourceUser,targetUser):
+        if not self.hasRelation(sourceUser,targetUser):
+             self.network[sourceUser].append(targetUser)
+
+
+    def removeRelation(self,sourceUser,targetUser):
+        if self.hasRelation(sourceUser,targetUser):
+             self.network[sourceUser].remove(targetUser)
